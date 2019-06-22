b1af72effbda12d77922af0836fb301326854f80
sujian3309 <sujian330@sina.com>
2019-06-22 20:36:09 +0800

    用户登陆-发送验证码-请求获取初始化验证码的参数

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,45 +1,85 @@
 <template>
-  <div class="login-warp">
-    <!-- 给组件加class，会把这个class作用到组件的根元素 -->
-    <el-form class="login-form" ref="form" :model="form">
-      <el-form-item>
-        <el-input v-model="form.mobile" placeholder="请输入验证码"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">登陆</el-button>
-      </el-form-item>
-    </el-form>
+  <div class="login-wrap">
+    <!-- 给组件加class 会把这个class会把这个class作用到组件的根元素上 -->
+    <div class="form-wrap">
+      <div class="form-head">
+        <img src="@/assets/logo_index.png" alt="黑马头条号">
+      </div>
+      <el-form class="form-content" ref="form" :model="form">
+        <el-form-item>
+          <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <!-- el-col 栅格布局，一共 24 列，:span 用来指定占用的大小，:offset 用来指定偏移量 -->
+          <el-col :span="14">
+            <el-input v-model="form.code" placeholder="验证码"></el-input>
+          </el-col>
+          <el-col :offset="1" :span="9">
+            <el-button @click="handleSendCode">获取验证码</el-button>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
+<script src=""></script>
 <script>
+import axios from 'axios'
 export default {
-  name: 'AppLogin',
+  name: "AppLogin",
   data() {
     return {
       form: {
-        mobile: '',
-        code: ''
+        mobile: "",
+        code: ""
       }
     };
   },
   methods: {
-    onSubmit() {
+    handleLogin() {
       console.log("submit!");
+    },
+    handleSendCode () {
+      const { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${ mobile }`
+      }).then(res => {
+        console.log(res.data);
+      },function(){
+
+      })
     }
   }
 };
 </script>
 <style lang="less" scoped>
-.login-warp {
+.login-wrap {
   height: 100%;
-  background-color: #e1f0ff;
+  // background-color: skyblue;
   display: flex;
   justify-content: center;
   align-items: center;
-  .login-from {
+  .form-head {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+    img {
+      width: 200px;
+    }
+  }
+  .form-wrap {
+    width: 450px;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 10px;
+    .btn-login {
+      width: 100%;
+    }
   }
 }
 </style>
